0424e18b0f3f1370dd07930093d35c7c942e2a76
almaz <almaz_73@mail.ru>
2019-01-01 11:06:05 +0300

    init

---
+++ src/pages/Authorize.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        Authorize
+        <p> Логин: <input></p>
+        <p> Пароль: <input></p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Authorize"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        INDEX !!!!!!!!!!!!!!!!!
+        <router-link to="/enter">Авторизоваться</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Index",
+        mounted(){
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
